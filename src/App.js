062bdd8f465c499694910f56a9b9ee08e65bc10f
Mina S. Aziz <minaz922@gmail.com>
2017-06-18 00:32:17 +0200

    add a mock component and emulate message sending

--- src/App.js
+++ src/App.js
@@ -1,18 +1,58 @@
 import React, { Component } from 'react';
+import io from 'socket.io-client';
 import logo from './logo.svg';
 import './App.css';
 
+const url = 'localhost:8000';
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      message: '',
+      messages: []
+    }
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    this.sock = io(url);
+  }
+
+  onChange(e) {
+    this.setState({message: e.target.value});
+    console.log(this.state.message);
+  }
+
+  onSubmit(e) {
+    this.setState({messages: this.state.messages.concat(this.state.message)});
+    e.preventDefault();
+    console.log(this.state.messages);
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>React Live Chat Example</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="chatbox-wrapper">
+          <div>
+           {this.state.messages.map((message) => {
+             return(
+               <div>
+                 {message}
+               </div>
+             )
+           })}
+          </div>
+          <form onSubmit={this.onSubmit}>
+            <input type="text" name="message" className="chatbox-input" value={this.state.message} onChange={this.onChange} />
+            <input type="submit" value="Send" />
+          </form>
+        </div>
       </div>
     );
   }
